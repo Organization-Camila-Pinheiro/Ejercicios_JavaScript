852ac305c5c723e27ba8242802b52bc08204e6c3
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-04-30 21:17:47 +0200

    Ejemplos Constantes y Operaciones

---
+++ js/ej3_operaciones.js
@@ -0,0 +1,60 @@
+// SUMAN DOS NUMEROS O MAS 
+
+let a = 2.5
+let b = 56
+let c = a + b 
+document.write("suma de a + b es igual a: " + c)
+document.write("<br/>")
+
+// RESTAR DOS NUMEROS 
+
+let descuento = 1
+let precioTomate = 2.8
+let precioTomateDescuento
+precioTomateDescuento = precioTomate - descuento
+document.write("El resultado de la resta del descuento de los tomates es: " +  precioTomateDescuento)
+document.write("<br/>")
+
+
+// MULTIPLICACION DE DOS NUEMEROS
+// calcular el precio de 5 kg de manzanas
+let kgManzana = 5
+let precioManzana = 3.56
+let totalMultiplicacion = precioManzana * kgManzana 
+document.write("El valor de  5 kg de manzanas es: " + totalMultiplicacion)
+document.write("<br/>")
+
+//DIVISION DE DOS NUMEROS
+//calcular la mitad del precio de las manzanas
+let mitad = 2
+let totalDivision = precioManzana / mitad
+document.write("El resultado de la division del precio las manzanas por la mitad es: " + totalDivision)
+document.write("<br/>")
+
+//COMBINACIÓN DE OPERACIONES 
+// usar parentesis en las operaciones que se deve hacer primero
+
+let notaExamen1 = 5.6
+let notaExamen2 = 8.2
+let notaExamen3 = 6.5
+let numeroExamen = 3
+let notaMedia
+
+notaMedia = (notaExamen1 + notaExamen2 + notaExamen3)/numeroExamen
+document.write("Combinar várias operaciones: La média de los examenes " + notaExamen1 + " " + notaExamen2 + " " + notaExamen3)
+document.write("<br/>")
+document.write("média de las notas es: " + notaMedia)
+document.write("<br/>")
+
+//OPERACIONES DE INCREMENTO Y DECREMENTO
+let numeroIntentos =  5
+document.write("El numero de intentos es " + numeroIntentos)
+document.write("<br/>")
+numeroIntentos++
+numeroIntentos++
+document.write("El numero de intentos incrementados es " + numeroIntentos)
+document.write("<br/>")
+numeroIntentos--
+document.write("El numero de intentos decrementados es " + numeroIntentos)
+document.write("<br/>")
+
