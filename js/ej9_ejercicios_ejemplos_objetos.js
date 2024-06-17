9e8fd77fadcd9f6d5028561d88fc83593201e722
Camila Pinheiro <161722302+cam-pinheiro@users.noreply.github.com>
2024-06-17 14:05:23 +0200

    Update ej9_ejercicios_ejemplos_objetos.js

--- js/ej9_ejercicios_ejemplos_objetos.js
+++ js/ej9_ejercicios_ejemplos_objetos.js
@@ -1,3 +1,4 @@
+// creamos el objeto mascota
 let mascota = {
     nombre: "Nevado",
     tipo: "Perro",
@@ -13,27 +14,29 @@ function imprimir(mensaje,id){
     parrafo.innerText =  mensaje;
 }
 
+// creamos un objeto mascota y imprimimos
 function mostrarObjeto(){
-
     let mensaje = "El nombre " + mascota.nombre + " Es un " + mascota.tipo + 
     " edad " + mascota.edad + " ha nascido en " + mascota.ciudad + " y su proprietario es " + mascota.proprietario
 
-    
     imprimir( mensaje, "ejemplo1")
 }
 
+// mostrar la informacion elegida del objeto mascota
 function mostrarPropriedad(){
     let propriedad = document.getElementById("ej2mascotas").value;
     let mensaje = propriedad + ": " + mascota[propriedad];
     imprimir(mensaje, "ejemplo2")
 }
 
+// cambiar elementos de un objeto. cambiar nombre
 function cambiarNombreMascota(){
     let nuevoNombre = document.getElementById("ej3nombre").value;
     mascota.nombre = nuevoNombre;
     mostrarObjeto();
 }
 
+// agregar nuevas propriedades a un objeto
 function agregarProprietario(){
     let nombreProprietario = document.getElementById("ej4propriedad").value;
     mascota["proprietario"] = nombreProprietario;
@@ -41,7 +44,6 @@ function agregarProprietario(){
 }
 
 // agreagar una nueva mascota a la lista
-
 function agregarNuevaMascota(){
     let miMascota = {};
     miMascota.nombre =  document.getElementById("ej5mascota").value;
@@ -52,8 +54,6 @@ function agregarNuevaMascota(){
 
     let mensaje = "Mascota agregada, la lista de mascotas es " + mascotas;
     imprimirArray();
-
-
 
 }
 
